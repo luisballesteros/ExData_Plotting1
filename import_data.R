d1d958a43b51a429fa87ac1e03a987d58682f59b
Luis Ballesteros <>
2021-03-24 20:45:34 +0100

    Unzip file in "data" directory. Create a "Data Frame" "df_all" with "fread" and select only the days of interest "df". Transform "Date" to date with "lubridate" library

--- import_data.R
+++ import_data.R
@@ -1,10 +1,26 @@
+library(plyr)
+library(dplyr)
+library(data.table)
+library(lubridate)
+
+# Create data folder if it does not exist
 if (!file.exists("data")) {
   dir.create("data")
 }
+# Download file to local "data" folder
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileUrl, destfile = "./data/household_power_consumption.zip", method = "curl")
 dateDownloaded <- date()
-# cameraData <- read.table("./data/cameras.csv", sep = ",", header = TRUE)
-#head(cameraData)
-# na.strings 
+dateDownloaded 
+# Unzip file in "data" directory
 unzip(zipfile = "./data/household_power_consumption.zip", exdir = "./data")
+# Create a "Data Frame" with "fread" and select only the days of interest.
+df_all <- fread("./data/household_power_consumption.txt", 
+                na.strings = c("?",NA))
+df <- subset(df_all, Date == "1/2/2007" | Date == "2/2/2007")
+# Transform "Date" to date with "lubridate" library
+df$Date <- dmy(df$Date)
+# Check "Data Frame" and delete data source from memory 
+head(df)
+str(df)
+rm(df_all)
